f54fbfd50525c4d401333064a71423234704bb25
Suhrob <suhrob_kalandarov@outlook.com>
2025-11-09 11:52:09 +0500

    Added new api logics to Catalog service

--- src/service/catalog.js
+++ src/service/catalog.js
@@ -22,6 +22,40 @@ const CategoryService = {
             throw error
         }
     },
+
+    async fetchCategories(selectedSpotlight) {
+        try {
+            let data
+            if (selectedSpotlight) {
+                data = await CategoryService.getCategoriesBySpotlightName(selectedSpotlight.name);
+            } else {
+                data = await CategoryService.getCategories();
+            }
+            localStorage.setItem("prime-categories", JSON.stringify(data))
+        } catch (error) {
+            console.error("Error fetching categories:", error)
+        }
+    },
+
+    async getCategoriesFromLS(selectedSpotlight) {
+        if (selectedSpotlight) {
+
+        } else {
+
+        }
+
+        const categories = localStorage.getItem("prime-categories");
+        if (categories) {
+            try {
+                const parsedCategories = JSON.parse(categories);
+                console.log("Parsed categories from localStorage:", parsedCategories);
+                return parsedCategories;
+            } catch (error) {
+                console.error("Failed to parse prime-categories:", error);
+                return CategoryService.fetchCategories(selectedSpotlight)
+            }
+        }
+    }
 }
 
 export default CategoryService
