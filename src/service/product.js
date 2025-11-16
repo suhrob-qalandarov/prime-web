3ee83421a696b3bfc55f243b905cc01300d81c4e
Suhrob <suhrob_kalandarov@outlook.com>
2025-11-16 09:43:57 +0500

    Added Product service getById function

--- src/service/product.js
+++ src/service/product.js
@@ -20,6 +20,16 @@ const ProductService = {
             throw error
         }
     },
+
+    async getProductById(productId) {
+        try {
+            const response = await axios.get(`/v2/product/${productId}`)
+            return response.data
+        } catch (error) {
+            console.error("Error fetching product:", error)
+            throw error
+        }
+    },
 }
 
 export default ProductService
